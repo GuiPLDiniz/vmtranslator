92ce738b64399e9c3aeac6438009ddfc08c471df
Guilherme Pessoa Lima Diniz <glimadiniz2@gmail.com>
2026-06-04 22:30:18 -0300

    feat(parser): implementa leitura e classificacao de comandos VM

--- src/main.py
+++ src/main.py
@@ -1,5 +1,25 @@
+from parser import Parser
+
+
 def main():
-    print("VMTranslator - Projeto iniciado")
+
+    parser = Parser("tests/Simple.vm")
+
+    while parser.has_more_commands():
+
+        parser.advance()
+
+        print(
+            parser.command_type(),
+            parser.arg1(),
+            end=""
+        )
+
+        if parser.command_type() != "C_ARITHMETIC":
+            print(f" {parser.arg2()}")
+
+        else:
+            print()
 
 
 if __name__ == "__main__":
--- src/parser.py
+++ src/parser.py
@@ -1,2 +1,64 @@
 class Parser:
-    pass
+
+    ARITHMETIC_COMMANDS = {
+        "add",
+        "sub",
+        "neg",
+        "eq",
+        "gt",
+        "lt",
+        "and",
+        "or",
+        "not"
+    }
+
+    def __init__(self, filename):
+
+        self.commands = []
+
+        with open(filename, "r", encoding="utf-8") as file:
+
+            for line in file:
+
+                line = line.split("//")[0].strip()
+
+                if line:
+                    self.commands.append(line)
+
+        self.current_index = -1
+        self.current_command = None
+
+    def has_more_commands(self):
+
+        return self.current_index + 1 < len(self.commands)
+
+    def advance(self):
+
+        self.current_index += 1
+        self.current_command = self.commands[self.current_index]
+
+    def command_type(self):
+
+        parts = self.current_command.split()
+
+        if parts[0] == "push":
+            return "C_PUSH"
+
+        if parts[0] == "pop":
+            return "C_POP"
+
+        if parts[0] in self.ARITHMETIC_COMMANDS:
+            return "C_ARITHMETIC"
+
+        raise ValueError(f"Comando desconhecido: {self.current_command}")
+
+    def arg1(self):
+
+        if self.command_type() == "C_ARITHMETIC":
+            return self.current_command.split()[0]
+
+        return self.current_command.split()[1]
+
+    def arg2(self):
+
+        return int(self.current_command.split()[2])
